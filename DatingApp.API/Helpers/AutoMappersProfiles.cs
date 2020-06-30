41a268ceb66f122fabcbe5e26bb76ca0665f2f71
work <kahanaomri@gmail.com>
2020-06-30 20:07:53 +0300

    end of section 8

---
+++ DatingApp.API/Helpers/AutoMappersProfiles.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMappersProfiles : Profile
+    {
+        public AutoMappersProfiles()
+        {
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.Age, opt => 
+                    opt.MapFrom(src => src.DateOfBirth.CalculateAge()))
+                .ForMember(dest => dest.PhotoUrl, opt => 
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.isMain).Url));
+            CreateMap<User, UserForDetailsDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => 
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.isMain).Url))
+                .ForMember(dest => dest.Age, opt => 
+                    opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotosForDetailsDto>();
+        }
+    }
+}
